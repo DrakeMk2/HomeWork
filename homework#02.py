eda8ad34fa07158e81b650789bfbfa962c2ed298
DrakeMk2 <doosiv@gmail.com>
2024-06-09 13:40:39 +0300

    Add files via upload

---
+++ homework#02.py
@@ -0,0 +1,5 @@
+homeworks_done=12
+hours_spent=1.5
+course_name='Python'
+homework_time=(hours_spent/homeworks_done)
+print('Курс:',course_name+',','всего задач:',homeworks_done,',','затрачено часов:',hours_spent,',','среднее время выполнения:',homework_time)
