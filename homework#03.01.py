a0f800124ba24b8aff8be20288b54f00c88e75b2
DrakeMk2 <doosiv@gmail.com>
2024-06-27 13:16:19 +0300

    Add files via upload

---
+++ homework#03.01.py
@@ -0,0 +1,33 @@
+calls = 0
+
+
+def count_calls():
+    global calls
+    calls += 1
+
+
+def string_info(string):
+    count_calls()
+    return {len(string), string.upper(), string.lower()}
+
+
+def is_contains(string, list_to_search):
+    count_calls()
+    for i in list_to_search:
+        if string.casefold() in i.casefold():
+            q = True
+            break
+        else:
+            q = False
+            continue
+    return q
+
+
+print(string_info('Bastion'))
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Cart', ['mart', 'baRT', 'DarTs']))
+print(is_contains('Form', ['fArm', 'UniforM', 'mOrF']))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(calls)
