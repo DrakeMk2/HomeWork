f5c44086be88d3044079bed03d2e5bd574fd7ba5
DrakeMk2 <doosiv@gmail.com>
2024-08-20 17:27:49 +0300

    Update homework#06.03.py
    -
    5    super().__init__()
    +
    22    def __init__(self):
    23    Horse.__init__(self)
    24    Eagle.__init__(self)

--- homework#06.03.py
+++ homework#06.03.py
@@ -2,7 +2,7 @@ class Horse:
     def __init__(self):
         self.x_distance = 0
         self.sound = 'Frrr'
-        super().__init__()
+        #super().__init__()
 
     def run(self, dx):
         self.x_distance += dx
@@ -18,6 +18,10 @@ class Eagle:
 
 
 class Pegasus(Horse, Eagle):
+
+    def __init__(self):
+        Horse.__init__(self)
+        Eagle.__init__(self)
 
     def move(self, dx, dy):
         self.run(dx)
