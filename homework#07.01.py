31dfb93c86c61e2500c1a17a783965f5eff7b8b9
DrakeMk2 <doosiv@gmail.com>
2024-08-29 19:35:49 +0300

    Add files via upload

---
+++ homework#07.01.py
@@ -0,0 +1,56 @@
+from pprint import pprint
+
+
+class Product:
+    def __init__(self, name, weight, category):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        return f'{self.name}, {self.weight}, {self.category}'
+
+
+class Shop:
+    def __init__(self):
+        self.__file_name = 'products.txt'
+
+    def get_products(self):
+        with open(self.__file_name, 'r') as file:
+            content = file.read()
+            return content
+
+    def add(self, *products):
+        with open(self.__file_name, 'a+') as file_app:
+            for product in products:
+                if product.name not in self.get_products():
+                    file_app.write(f'{str(product)}\n')
+                else:
+                    print(f'Продукт {product.name} уже есть в магазине')
+
+
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2)  # __str__
+
+s1.add(p1, p2, p3)
+
+print(s1.get_products())
+
+# Первый запуск:
+# Spaghetti, 3.4, Groceries
+# Potato, 50.5, Vegetables
+# Spaghetti, 3.4, Groceries
+# Potato, 5.5, Vegetables
+# Второй запуск:
+# Spaghetti, 3.4, Groceries
+# Продукт Potato, 50.5, Vegetables уже есть в магазине
+# Продукт Spaghetti, 3.4, Groceries уже есть в магазине
+# Продукт Potato, 5.5, Vegetables уже есть в магазине
+# Potato, 50.5, Vegetables
+# Spaghetti, 3.4, Groceries
+# Potato, 5.5, Vegetables
