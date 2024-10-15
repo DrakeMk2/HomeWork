90ec66c4906364561143d73da586a5d5df57b49d
DrakeMk2 <doosiv@gmail.com>
2024-10-15 17:32:07 +0300

    Update homework#12.01.py

--- homework#12.01.py
+++ homework#12.01.py
@@ -7,13 +7,13 @@ class RunnerTest(TestCase):
         r1 = Runner('Иван')
         for i in range(10):
             r1.walk()
-        self.assertEquals(r1.distance, 50)
+        self.assertEqual(r1.distance, 50)
 
     def test_run(self):
         r2 = Runner('Павел')
         for i in range(10):
             r2.run()
-        self.assertEquals(r2.distance, 100)
+        self.assertEqual(r2.distance, 100)
 
     def test_challenge(self):
         r1 = Runner('Иван')
@@ -21,4 +21,4 @@ class RunnerTest(TestCase):
         for i in range(10):
             r1.run()
             r2.walk()
-        self.assertNotEquals(r1.distance, r2.distance)
+        self.assertNotEqual(r1.distance, r2.distance)
