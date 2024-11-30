c1c0dbee4dbd3127c8a9d14eeccebd1d71a0098e
DrakeMk2 <doosiv@gmail.com>
2024-11-30 17:25:45 +0300

    Update user.py

--- app_v4/routers/user.py
+++ app_v4/routers/user.py
@@ -19,7 +19,7 @@ async def all_users(db: Annotated[Session, Depends(get_db)]):
 @router.get('/user_id')
 async def user_by_id(db: Annotated[Session, Depends(get_db)],
                      user_id: int):
-    user = db.scalars(select(User).where(User.id == user_id))
+    user = db.scalar(select(User).where(User.id == user_id))
     if user is not None:
         return user
     raise HTTPException(status_code=404, detail='User was not found')
@@ -41,7 +41,7 @@ async def create_user(db: Annotated[Session, Depends(get_db)],
 async def update_user(db: Annotated[Session, Depends(get_db)],
                       user_id: int,
                       user_update_model: UpdateUser):
-    user = db.scalars(select(User).where(User.id == user_id))
+    user = db.scalar(select(User).where(User.id == user_id))
     if user is not None:
         db.execute(update(User).where(User.id == user_id).values(
             username=user_update_model.username,
@@ -58,7 +58,7 @@ async def update_user(db: Annotated[Session, Depends(get_db)],
 @router.delete('/delete')
 async def delete_user(db: Annotated[Session, Depends(get_db)],
                       user_id: int):
-    user = db.scalars(select(User).where(User.id == user_id))
+    user = db.scalar(select(User).where(User.id == user_id))
     if user is not None:
         db.execute(delete(User).where(User.id == user_id))
         db.commit()
